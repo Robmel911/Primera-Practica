aa35977ca3651dea07a0011b98d6924f630d422e
Robmel <elrobmon@gmail.com>
2026-02-27 22:34:21 -0400

    Cambios de nombres y creacion de la Conexion a la DB

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
-namespace CapaDatos
-{
-    public class Class1
+
+   
+    namespace CapaDatos
     {
+        public class Conexion
+        {
+            private SqlConnection conexion = new SqlConnection("Server=.;Database=SistemaColmado;Integrated Security=True");
+
+            public SqlConnection AbrirConexion()
+            {
+                if (conexion.State == ConnectionState.Closed)
+                    conexion.Open();
+                return conexion;
+            }
+
+            public SqlConnection CerrarConexion()
+            {
+                if (conexion.State == ConnectionState.Open)
+                    conexion.Close();
+                return conexion;
+            }
+        }
     }
-}
+
