30d91d5dae3828c5aecab30923fa988e37d99357
Robmel <elrobmon@gmail.com>
2026-03-27 18:09:22 -0400

    Modulo 1 Parte 1

---
+++ Primera Practica/Login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primera_Practica
+{
+    
+    public partial class Login : Form
+    {
+        private Principal inicio = new Principal();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            MessageBox.Show("Bienvenido " + txtUsuario.Text);
+            inicio.FormClosed += (s, args) => Application.Exit();
+            inicio.Show();
+            this.Hide();
+
+        }
+    }
+}
--- Primera Practica/Program.cs
+++ Primera Practica/Program.cs
@@ -16,7 +16,7 @@ namespace Primera_Practica
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Principal());
+            Application.Run(new Login());
         }
     }
 }
